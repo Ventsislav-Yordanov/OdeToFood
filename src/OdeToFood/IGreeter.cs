62f0a35fa00c33976f92480b6cceef28dd2d314b
Ventsislav Yordanov <venciityjordanov@gmail.com>
2016-10-06 18:19:16 +0300

    Creating a greeting service.

---
+++ src/OdeToFood/IGreeter.cs
@@ -0,0 +1,7 @@
+﻿namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetGreeting();
+    }
+}
--- src/OdeToFood/Startup.cs
+++ src/OdeToFood/Startup.cs
@@ -29,10 +29,16 @@ namespace OdeToFood
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(this.Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env,
+            ILoggerFactory loggerFactory,
+            IGreeter greeter)
         {
             loggerFactory.AddConsole();
 
@@ -43,7 +49,7 @@ namespace OdeToFood
 
             app.Run(async (context) =>
             {
-                var message = this.Configuration["Greeting"];
+                var message = greeter.GetGreeting();
                 await context.Response.WriteAsync(message);
             });
         }
