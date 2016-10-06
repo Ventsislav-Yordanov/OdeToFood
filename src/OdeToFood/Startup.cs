f0ec538e7306de38b3ded804380fea5955d16097
Ventsislav Yordanov <venciityjordanov@gmail.com>
2016-10-06 17:51:41 +0300

    Adding a configuration file ( add missing files )

--- src/OdeToFood/Startup.cs
+++ src/OdeToFood/Startup.cs
@@ -7,11 +7,24 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+
+            this.Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -30,7 +43,8 @@ namespace OdeToFood
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var message = this.Configuration["Greeting"];
+                await context.Response.WriteAsync(message);
             });
         }
     }
