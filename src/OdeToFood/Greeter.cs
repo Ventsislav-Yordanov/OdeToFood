3b39dc3ba6c9de6f00cade76d89a34329e888d3b
Ventsislav Yordanov <venciityjordanov@gmail.com>
2016-10-06 18:25:18 +0300

    Commit the Greeter file ( again... )

---
+++ src/OdeToFood/Greeter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public class Greeter : IGreeter
+    {
+        private string greeting;
+
+        public Greeter(IConfiguration configuration)
+        {
+            this.greeting = configuration["Greeting"];
+        }
+
+        public string GetGreeting()
+        {
+            return this.greeting;
+        }
+    }
+}
