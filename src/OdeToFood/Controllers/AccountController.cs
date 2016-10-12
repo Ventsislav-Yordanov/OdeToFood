be5428e4becf00ae11de6473e6f2c5cabc50ba38
Ventsislav Yordanov <venciityjordanov@gmail.com>
2016-10-13 02:16:10 +0300

    Added the user registraion form
    Added new controller "AccountController". Added view model for the
    registration for a user. Also added view that displays the form for
    registration. And add using import for the view models in the
    _ViewImports.cshtml file.

---
+++ src/OdeToFood/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.ViewModels;
+
+namespace OdeToFood.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterUserViewModel model)
+        {
+            // TODO: Add the creation of the user
+        }
+    }
+}
